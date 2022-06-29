28c69b80c0ec9c82ff3a07b0314abb6306ccce37
o-ddong <odh0112@naver.com>
2022-06-29 19:53:51 +0900

    [BOJ] 기타줄 / S4 / https://www.acmicpc.net/problem/1049

--- Greedy Algorithm/1049.py
+++ Greedy Algorithm/1049.py
@@ -1,9 +1,7 @@
 # S4 기타줄
 
-# 서로 다른 브랜드끼리 살 수 있는거를 고려를 하지 못함
-
-
 # 1번 풀이 (실패)
+# 서로 다른 브랜드끼리 살 수 있는거를 고려를 하지 못함
 n, m = map(int, input().split())
 result = []
 
@@ -16,3 +14,19 @@ for i in range(m):
         result.append(min((n // 6 + 1) * package, one * n, (n // 6 * package) + n % 6 * one))
 
 print(min(result))
+
+# 2번 풀이 (성공)
+
+n, m = map(int, input().split())
+packageMinValue = 1001
+oneMinValue = 1001
+
+for i in range(m):
+    package, one = map(int, input().split())
+    packageMinValue = min(packageMinValue, package) 
+    oneMinValue = min(oneMinValue, one)
+
+if n <= 6:
+    print(min(packageMinValue, oneMinValue * n))
+else:
+    print(min((n // 6 + 1) * packageMinValue, oneMinValue * n ,(n // 6 * packageMinValue) + (n % 6) * oneMinValue))
