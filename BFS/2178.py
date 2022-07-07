fc3d1cc7fb68f9df1dcd6037a039691f8a6a26eb
o-ddong <odh0112@naver.com>
2022-07-07 13:51:54 +0900

    [BOJ] 미로 탐색 / S1 / https://www.acmicpc.net/problem/2178

--- BFS/2178.py
+++ BFS/2178.py
@@ -1,4 +1,8 @@
 # S1 미로 탐색
+
+# 1번 풀이 (성공)
+# 다만 방문 checking을 할 필요가 없음
+
 from collections import deque
 
 def bfs():
@@ -30,3 +34,39 @@ visited = [([0] * m) for _ in range(n)]
 bfs()
 
 print(graph[n-1][m-1])
+
+# 2번 풀이 (성공)
+
+from collections import deque
+
+n, m = map(int, input().split())
+
+# 칸 입력
+graph = [list(map(int, input())) for _ in range(n)]
+
+def bfs(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    q = deque()
+    q.append((x, y))
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+            
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
